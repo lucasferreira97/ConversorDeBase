38d15dec894af9ca6c22584007a7259d0c42afd5
julioTrindade1995 <30849513+julioTrindade1995@users.noreply.github.com>
2017-08-17 20:36:44 -0300

    commit unit tests

--- ConversorDeBase/test/ConversorTest.java
+++ ConversorDeBase/test/ConversorTest.java
@@ -4,41 +4,97 @@
  * and open the template in the editor.
  */
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import conversores.Conversor;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author lucas97.silva
- */
 public class ConversorTest {
-    
-    public ConversorTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testBinToDec() {
+        final String valor = "11010";
+        final int conversao = Conversor.binToDec(valor);
+        assertTrue(conversao == 26);
+    }
+    
+    @Test
+    public void testBinToHex() {
+        final String valor = "11010";
+        final String conversao = Conversor.binToHex(valor);
+        assertTrue(conversao.equals("1a"));
+    }
+    
+    @Test
+    public void testBinToOct() {
+        final String valor = "11010";
+        final String conversao = Conversor.binToOct(valor);
+        assertTrue(conversao.equals("32"));
+    }
+    
+    @Test
+    public void testDecToBin() {
+        final int valor = 26;
+        final String conversao = Conversor.decToBin(valor);
+        assertTrue(conversao.equals("11010"));
+    }
+    
+    @Test
+    public void testDecToHex() {
+        final int valor = 26;
+        final String conversao = Conversor.decToHex(valor);
+        assertTrue(conversao.equals("1a"));
+    }
+    
+    @Test
+    public void testDecToOct() {
+        final int valor = 26;
+        final String conversao = Conversor.decToOct(valor);
+        assertTrue(conversao.equals("32"));
+   
+    }
+    
+    @Test
+    public void testHexToBin() {
+        final String valor = "1a";
+        final String conversao = Conversor.hexToBin(valor);
+        assertTrue(conversao.equals("11010"));
+    }
+    
+    @Test
+    public void testHexToDec() {
+        final String valor = "1a";
+        final int conversao = Conversor.hexToDec(valor);
+        assertTrue(conversao == 26);
+    }
+    
+    @Test
+    public void testHexToOct() {
+        final String valor = "1a";
+        final String conversao = Conversor.hexToOct(valor);
+        assertTrue(conversao.equals("32"));
+    }
+    
+    @Test
+    public void testOctToBin() {
+        final String valor = "32";
+        final String conversao = Conversor.octToBin(valor);
+        assertTrue(conversao.equals("11010"));
+    }
+    
+    @Test
+    public void testOctToDec() {
+        final String valor = "32";
+        final int conversao = Conversor.octToDec(valor);
+        assertTrue(conversao == 26);
+    }
+    
+    @Test
+    public void testOctToHex() {
+        final String valor = "32";
+        final String conversao = Conversor.octToHex(valor);
+        assertTrue(conversao.equals("1a"));
+    }
+    
+    
+            
 }
